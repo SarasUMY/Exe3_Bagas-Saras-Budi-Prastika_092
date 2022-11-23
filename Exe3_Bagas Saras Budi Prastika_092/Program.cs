0e869835465fd6365fb08bd54732903e997d9a3c
TA1092 <bagas.saras.ft21@mail.umy.ac.id>
2022-11-23 15:32:57 +0700

    Add project files.

---
+++ Exe3_Bagas Saras Budi Prastika_092/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe3_Bagas_Saras_Budi_Prastika_092
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
