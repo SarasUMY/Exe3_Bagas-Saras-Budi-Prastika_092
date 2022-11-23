05100a7285d95595ae5dcf299b7f04d085dfaf6b
TA1092 <bagas.saras.ft21@mail.umy.ac.id>
2022-11-23 16:02:07 +0700

    membuat perulangan listEmpty

--- Exe3_Bagas Saras Budi Prastika_092/Program.cs
+++ Exe3_Bagas Saras Budi Prastika_092/Program.cs
@@ -46,7 +46,20 @@ namespace Exe3_Bagas_Saras_Budi_Prastika_092
         /*traverses all the node of the list*/
         public void traverse()
         {
-            
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+            {
+                Console.WriteLine("\nRecords in the list are:\n");
+                Node currentNode;
+                currentNode = LAST.next;
+                while (currentNode != LAST)
+                {
+                    Console.Write(currentNode.rollNumber + "    " + currentNode.name + "\n");
+                    currentNode = currentNode.next;
+                }
+                Console.Write(LAST.rollNumber + "    " + LAST.name + "\n");
+            }
         }
     }
 }
